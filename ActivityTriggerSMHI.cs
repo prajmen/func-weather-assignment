1133ebb4ec4b834f37d459aa3b6ff4748091bf03
prajmen <70475045+prajmen@users.noreply.github.com>
2022-10-26 22:05:06 +0200

    add httpclient in ActivityTriggerSMHI.cs

--- ActivityTriggerSMHI.cs
+++ ActivityTriggerSMHI.cs
@@ -6,17 +6,28 @@ using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-
+using System.Text.Json;
 
 namespace WeatherAssignment
 {
     public static class ActivityTriggerSMHI 
     {
+        //För best practice, borde httpClient injectas
+        //https://learn.microsoft.com/en-us/azure/azure-functions/functions-dotnet-dependency-injection
+        private static HttpClient httpClient = new HttpClient();
+
         [FunctionName(nameof(GetWeatherDataSMHI))]
-        public static string GetWeatherDataSMHI([ActivityTrigger] string name, ILogger log)
+        public static async Task<string> GetWeatherDataSMHI([ActivityTrigger] string name, ILogger log)
         {
+            var url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json";
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = response.Content.ReadAsStringAsync().Result;
+            }
+            
             log.LogInformation($"Saying hello to {name}.");
-            return $"Hello {name}!";
+            return $"Hello!";
         }
     }
 }
